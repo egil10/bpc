98498e9d8ab812f57737358b4373889f0f75e851
egil10 <egilfure@gmail.com>
2025-11-16 01:48:10 +0100

    now added cpp!

---
+++ script/cpp/hello_there.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Hva heter du? ";
+    cin >> name;
+
+    cout << "\nVelkommen til C++, " << name << "!" << endl;
+    cout << "Du kjører C++ på Huawei MateBook D14 i Norge!" << endl;
+    cout << "Klokken er ca. 01:40 AM, 16. november 2025." << endl;
+
+    return 0;
+}
